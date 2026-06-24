78285a91fdeefbb55970dfab95538d224838bfd1
Minh Anh <daylaminhanhne555@gmail.com>
2026-06-24 12:34:03 +0700

    refactor: func reset

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const p1= {
+const p1 = {
     score: 0,
     button: document.querySelector('#p1Button'),
     display: document.querySelector('#p1Display')
 }
 
-const p2= {
+const p2 = {
     score: 0,
     button: document.querySelector('#p2Button'),
     display: document.querySelector('#p2Display')
@@ -22,7 +22,7 @@ let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-function updateScores(player, opponent){
+function updateScores(player, opponent) {
     if (!isGameOver) {
         player.score += 1;
         if (player.score === winningScore) {
@@ -38,11 +38,11 @@ function updateScores(player, opponent){
 
 
 p1.button.addEventListener('click', function () {
-    updateScores(p1,p2);
+    updateScores(p1, p2);
 })
 
 p2.button.addEventListener('click', function () {
-    updateScores(p2,p1);
+    updateScores(p2, p1);
 })
 
 winningScoreSelect.addEventListener('change', function () {
@@ -54,12 +54,10 @@ resetButton.addEventListener('click', reset);
 
 function reset() {
     isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-    p1Display.classList.remove('has-text-success', 'has-text-danger');
-    p2Display.classList.remove('has-text-success', 'has-text-danger');
-    p1Button.disabled = false;
-    p2Button.disabled = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
 }
